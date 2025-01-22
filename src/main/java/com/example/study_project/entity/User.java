111f8975a924321bf3d3295642dbdb1eb43d0139
hanjihoon03 <gkswlgns7653@gmail.com>
2025-01-22 21:25:52 +0900

    user 기본 권한 설정 추가

--- src/main/java/com/example/study_project/entity/User.java
+++ src/main/java/com/example/study_project/entity/User.java
@@ -16,6 +16,16 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true)
     private String username;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private UserRoleEnum role;
 }
