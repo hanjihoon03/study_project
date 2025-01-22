381380e183ea94caa08bd11f742fbfce4a35e4cc
hanjihoon03 <gkswlgns7653@gmail.com>
2025-01-22 20:58:21 +0900

    first commit

---
+++ src/test/java/com/example/study_project/StudyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.study_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
