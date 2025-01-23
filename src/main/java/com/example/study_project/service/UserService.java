22be5286178c245e68122b03029e77371044cfbf
hanjihoon03 <gkswlgns7653@gmail.com>
2025-01-23 21:01:00 +0900

    로그인 구현

--- src/main/java/com/example/study_project/service/UserService.java
+++ src/main/java/com/example/study_project/service/UserService.java
@@ -1,4 +1,50 @@
 package com.example.study_project.service;
 
+import com.example.study_project.dto.SignupRequestDto;
+import com.example.study_project.entity.User;
+import com.example.study_project.entity.UserRoleEnum;
+import com.example.study_project.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+
+    // ADMIN_TOKEN
+    private final String ADMIN_TOKEN = "AAABnvxRVklrnYxKZ0aHgTBcXukeZygoC";
+
+    public void signup(SignupRequestDto signupRequestDto) {
+        String username = signupRequestDto.getUsername();
+        String password = passwordEncoder.encode(signupRequestDto.getPassword());
+
+        Optional<User> checkUsername = userRepository.findByUsername(username);
+        if (checkUsername.isPresent()) {
+            throw new IllegalArgumentException("중복된 사용자가 존재합니다.");
+        }
+
+        String email = signupRequestDto.getEmail();
+        Optional<User> checkEmail = userRepository.findByEmail(signupRequestDto.getEmail());
+        if (checkEmail.isPresent()) {
+            throw new IllegalArgumentException("중복된 이메일 입니다.");
+        }
+
+        UserRoleEnum role = UserRoleEnum.USER;
+        if (signupRequestDto.isAdmin()) {
+            if (!ADMIN_TOKEN.equals(signupRequestDto.getAdminToken())) {
+                throw new IllegalArgumentException("관리자 암호가 틀려 등록이 불가합니다.");
+            }
+            role = UserRoleEnum.ADMIN;
+        }
+
+        User user = new User(username, password, email, role);
+        userRepository.save(user);
+    }
+
 }
